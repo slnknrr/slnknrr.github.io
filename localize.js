64ff4e125c09148d86c4fdb95a68ebbbc0185a20
Yuriyi `niknils` Slinkin <152192045+slnknrr@users.noreply.github.com>
2023-12-11 04:10:57 +0300

    localize experiment meme

---
+++ localize.js
@@ -0,0 +1,11 @@
+function localize(locales) { //0.1.0; TODO: force lang and object->text->regexp->custum tags get
+	var locale = Intl.DateTimeFormat().resolvedOptions().locale.replace(/^z(h|n)-CN$/i, 'zn-CN');
+	var page=window.location.toString().replace(/\/$/,'/index.html');
+
+	if (locales.includes(locale)) {	
+		var localize=page.replace(/\.html$/i,`.${locale}.htm`);
+	} else {
+		var localize=page.replace(/\.html$/i,`.${locales[0]}.htm`);
+	}
+	document.getElementById('main').innerHTML=`<object data="${localize}">`;
+}
